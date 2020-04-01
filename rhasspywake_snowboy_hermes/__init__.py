7e3c9aa52fc0cbccde1e87c1c6750a706c8ebd60
Michael Hansen <michael.hansen.24@us.af.mil>
2020-04-01 16:45:50 -0400

    Add disabled reasons

--- rhasspywake_snowboy_hermes/__init__.py
+++ rhasspywake_snowboy_hermes/__init__.py
@@ -79,6 +79,7 @@ class WakeHermesMqtt(HermesClient):
         self.model_dirs = model_dirs or []
 
         self.enabled = enabled
+        self.disabled_reasons: typing.Set[str] = set()
 
         # Required audio format
         self.sample_rate = sample_rate
@@ -295,11 +296,16 @@ class WakeHermesMqtt(HermesClient):
         """Received message from MQTT broker."""
         # Check enable/disable messages
         if isinstance(message, HotwordToggleOn):
-            self.enabled = True
-            self.first_audio = True
-            _LOGGER.debug("Enabled")
+            self.disabled_reasons.discard(message.reason)
+            if self.disabled_reasons:
+                _LOGGER.debug("Still disabled: %s", self.disabled_reasons)
+            else:
+                self.enabled = True
+                self.first_audio = True
+                _LOGGER.debug("Enabled")
         elif isinstance(message, HotwordToggleOff):
             self.enabled = False
+            self.disabled_reasons.add(message.reason)
             _LOGGER.debug("Disabled")
         elif isinstance(message, AudioFrame):
             if self.enabled:
